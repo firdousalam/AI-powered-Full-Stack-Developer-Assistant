69e308ff974107957651fb582e54dda3c1db97f7
Firdous Alam <firdous.alam2058@gmail.com>
2026-07-25 13:35:26 +0530

    feat(extension): initialize Manifest V3 project

--- chrome-extension/src/App.tsx
+++ chrome-extension/src/App.tsx
@@ -3,13 +3,18 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import "tailwindcss";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <div className="text-red-500 text-4xl">
+        Hello DevPilot
+      </div>
       <section id="center">
+
         <div className="hero">
           <img src={heroImg} className="base" width="170" height="179" alt="" />
           <img src={reactLogo} className="framework" alt="React logo" />
