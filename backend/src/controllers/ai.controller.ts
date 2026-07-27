e6bfb4acd5f89a1408ca116fc07addd5460f6874
Firdous Alam <firdous.alam2058@gmail.com>
2026-07-27 16:35:42 +0530

    Milestone 5 ended

--- backend/src/controllers/ai.controller.ts
+++ backend/src/controllers/ai.controller.ts
@@ -1,18 +1,30 @@
 import { Request, Response } from "express";
 
 export const chat = (
+
     req: Request,
+
     res: Response
+
 ) => {
 
-    console.log(req.body);
-    const { prompt } = req.body;
+    const {
+
+        prompt,
+
+        model
+
+    } = req.body;
+
+    console.log(prompt);
+
+    console.log(model);
 
     res.json({
 
         success: true,
 
-        response: `Mock AI Response for: ${prompt}`
+        response: "Hello from DevPilot Backend"
 
     });
 
